f9728ceb03fc932e512e7b21fb46c032763ac3a2
Ssimas3 <sstasytis@gmail.com>
2018-04-21 13:17:07 +0300

    Added Google Auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
-import RaisedButton from 'material-ui/RaisedButton';
 import { connect } from 'react-redux'
 import { startLogin } from '../actions/auth'
+import { Paper, RaisedButton, Card, CardMedia, ToolbarTitle, Toolbar, Slider } from 'material-ui';
+
 
 export class LoginComponent extends Component {
+    state = {
+        zDepth: 0
+    }
+    handleMouseEnter = () => {
+        this.setState({ zDepth: 2 })
+    }
+
+    handleMouseLeave = () => {
+        this.setState({ zDepth: 0 })
+    }
+
     render() {
         return (
             <div className="container">
-                <RaisedButton onClick={this.props.startLogin} label="Login" primary={true} />
+
+                <Paper zDepth={this.state.zDepth}
+                    onMouseEnter={this.handleMouseEnter}
+                    onMouseLeave={this.handleMouseLeave} >
+                    <Toolbar style={{ "justify-content": "center" }}>
+                        <ToolbarTitle text="Expensify App" />
+                    </Toolbar>
+                    <div style={{ textAlign: 'center', width: '100%', margin: '20px auto', padding: '20px auto' }}>
+
+                        Track your expenses
+                    </div>
+                    <div style={{ width: '50%', margin: '20px auto 20px auto' }}>
+
+                        <RaisedButton onClick={this.props.startLogin} label="Login" primary={true} />
+                    </div>
+
+
+                </Paper>
+
+
+
             </div>
         )
     }
